b6dc60173285a1bd87203f3bcfe88b73690deeaf
Jeff Downing <jeff.downing@latchd.com>
2014-11-19 13:19:34 -0500

    Added import code

--- src/main/java/org/codeaffectionado/training/jackrabbit_tutorial/App.java
+++ src/main/java/org/codeaffectionado/training/jackrabbit_tutorial/App.java
@@ -1,12 +1,17 @@
 package org.codeaffectionado.training.jackrabbit_tutorial;
 
-import javax.jcr.LoginException;
+import java.io.FileInputStream;
+
+import javax.jcr.ImportUUIDBehavior;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
+import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
+import javax.jcr.Value;
 
 import org.apache.jackrabbit.core.TransientRepository;
 
@@ -16,34 +21,59 @@ import org.apache.jackrabbit.core.TransientRepository;
  */
 public class App 
 {
-    public static void main( String[] args ) throws LoginException, RepositoryException
+    public static void main( String[] args ) throws Exception
     {
     	Repository repo = new TransientRepository();
     	Session session = repo.login(
     			new SimpleCredentials("admin","admin".toCharArray()));
     	Node root = session.getRootNode();
+    
     	
-    	if(!root.hasNode("content")){
-	    	Node contentNode = root.addNode("content");
-	    	Node peopleNode = contentNode.addNode("people");
-	    	Node jsmith = peopleNode.addNode("jsmith");
-	    	
-	    	jsmith.setProperty("firstname", "john");
-	    	jsmith.setProperty("age", 43);
-	    	
+    	if(!root.hasNode("catalog")){
+	    	Node contentNode = root.addNode("catalog");
+	    	FileInputStream stream = new FileInputStream("tutorial.xml");
+	        session.importXML(contentNode.getPath(),stream,
+	        		ImportUUIDBehavior.IMPORT_UUID_CREATE_NEW);
+	        stream.close();
 	    	session.save();
     	}else{
-    		Node contentNode = root.getNode("content");
-    		contentNode.remove();
-    		session.save();
+    		//Node contentNode = root.getNode("content");
+    		//contentNode.remove();
+    		//session.save();
     	}
     	
-    	NodeIterator i = root.getNodes();
-    	while(i.hasNext()){
-    		Node node = i.nextNode();
-    		System.out.println(node.getName());
-    	}
-    	
-        session.logout();
+    	dumpToConsole(root);
+    }
+    
+    public static void dumpToConsole(Node node) throws RepositoryException {
+
+        System.out.println(node.getPath());
+
+        if(node.hasProperties()){
+            PropertyIterator props = node.getProperties();
+            while(props.hasNext()){
+                Property property = (Property) props.next();
+
+                if(property.isMultiple()){
+                    Value[] values = property.getValues();
+                    System.out.print(property.getPath()+"="+"[");
+
+                    for(Value value : values){
+                        System.out.print(value.getString()+",");
+                    }
+                    System.out.println("]");
+                } else {
+                    System.out.println(property.getPath()+ "="+property.getString());
+                }
+            }
+        }
+
+        NodeIterator iterator = node.getNodes();
+
+        while(iterator.hasNext()){
+            Node next = (Node) iterator.next();
+
+            dumpToConsole(next);
+        }
     }
 }
